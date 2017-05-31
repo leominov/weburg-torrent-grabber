60d84c1dbe2fedbb581da676e89d05b27309c79c
leoderoko <leoderoko@gmail.com>
2017-05-31 12:33:01 +0500

    Rename del script variables

--- del_old_bt.sh
+++ del_old_bt.sh
@@ -6,18 +6,18 @@ set -ex
 # и удаляет торрент и файл.
 
 REMOTE=$(cat settings.json | jq -re '.transmission_remote')
-LOGIN=$(cat settings.json | jq -re '.transmission_remote_username')
-PASSWD=$(cat settings.json | jq -re '.transmission_remote_password')
+USERNAME=$(cat settings.json | jq -re '.transmission_remote_username')
+PASSWORD=$(cat settings.json | jq -re '.transmission_remote_password')
 TTL=$(cat settings.json | jq -re '.ttl')
 
 listing() {
-    $("$REMOTE" -n "$LOGIN:$PASSWD" -l | awk '{print $1}' | grep -v 'ID' | grep -v 'Sum:')
+    $("$REMOTE" -n "$USERNAME:$PASSWORD" -l | awk '{print $1}' | grep -v 'ID' | grep -v 'Sum:')
 }
 bt_search() {
-    $("$REMOTE" -n "$LOGIN:$PASSWD" -t "$NAME" -i | grep 'Seeding Time' | awk '{print $3}')
+    $("$REMOTE" -n "$USERNAME:$PASSWORD" -t "$NAME" -i | grep 'Seeding Time' | awk '{print $3}')
 }
 remove() {
-    $("$REMOTE" -n "$LOGIN:$PASSWD" -t "$NAME" --remove-and-delete 2>&1 > /dev/null)
+    $("$REMOTE" -n "$USERNAME:$PASSWORD" -t "$NAME" --remove-and-delete 2>&1 > /dev/null)
 }
 
 for NAME in $(listing); do
